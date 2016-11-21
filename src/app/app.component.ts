cdc526da618f3196b80ab1d482f603f92b02501e
chapini <aarontoledoa@yahoo.com>
2016-11-21 01:49:54 -0600

    This shouldn't work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component , Inject} from "@angular/core";
+import {AngularFire, FirebaseObjectObservable,FirebaseAuth} from 'angularfire2';
+import { AuthService } from './auth.service';
+
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = 'app works!';
+
+  user:any;
+
+  constructor( @Inject(FirebaseAuth) public auth: FirebaseAuth, private authserv: AuthService){
+this.user = this.authserv.uid;
+
+  }
+
+logout(){
+  this.authserv.signOut();
 }
+
+  }
+
+  
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,60 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {routing} from './app.routing';
 
 import { AppComponent } from './app.component';
+import { MaterializeModule } from 'angular2-materialize';
+import { HomeComponent } from './home.component';
+import {AngularFireModule} from 'angularfire2';
+import { AuthGuard } from './auth.guard';
+import { AuthService } from './auth.service';
+import { DispComponent } from './disp.component';
+import { LavaComponent } from './dispos/lava.component';
+import { TvComponent } from './dispos/tv.component';
+import { ClimaComponent } from './dispos/clima.component';
+import { TVTBComponent } from './tablas/tvtb.component';
+import { ClimaTBComponent } from './tablas/climatb.component';
+import { LavaTBComponent } from './tablas/lavatb.component';
+
+import { DataTableModule } from 'angular2-datatable';
+import { DataService } from './data.service';
+
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { RefriComponent } from './dispos/refri.component';
+import { RefriTBComponent } from './tablas/refritb.component';
+
+
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCzfTwp-r1yj0oDkId7MNm2-Zyf12kwhU8",
+    authDomain: "con100t-f2921.firebaseapp.com",
+    databaseURL: "https://con100t-f2921.firebaseio.com",
+    storageBucket: "con100t-f2921.appspot.com",
+    messagingSenderId: "263218796151"
+
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DispComponent,
+    LavaComponent,TvComponent, ClimaComponent,RefriComponent,
+    TVTBComponent, ClimaTBComponent, LavaTBComponent, RefriTBComponent
+  
   ],
-  imports: [
+  imports: [routing,
     BrowserModule,
     FormsModule,
-    HttpModule
+   DataTableModule,
+    HttpModule,
+     ChartsModule,
+MaterializeModule,
+ AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [AuthGuard,AuthService,DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
